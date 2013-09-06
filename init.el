69c1e7b59f22d0f897313f662748ea2c4aacbc33
Brett Dargan <brett.dargan@gmail.com>
2013-09-06 20:36:19 +1000

    tweak

--- init.el
+++ init.el
@@ -5,3 +5,17 @@
 
 ;; Load bindings config
 (live-load-config-file "bindings.el")
+
+(defconst +home-dir+ "~")
+(defconst +emacs-dir+ (concat +home-dir+ "/.live-packs/dargs-pack"))
+;;(defconst +emacs-profiles-dir+ (concat +emacs-dir+ "/profiles"))
+(defconst +emacs-lib-dir+ (concat +emacs-dir+ "/libs"))
+(defconst +emacs-conf-dir+ (concat +emacs-dir+ "/config"))
+;;(defconst +emacs-tmp-dir+ (concat +emacs-dir+ "/tmp"))
+
+(defun add-lib-path (p)
+  (add-to-list 'load-path (concat +emacs-lib-dir+ "/" p)))
+
+(live-load-config-file "js2.el")
+;;(live-load-config-file "js2-refactor.el")
+(live-load-config-file "coffee-mode.el")
